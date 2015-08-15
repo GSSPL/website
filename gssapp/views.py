9896a03db647b36a785599f9d4e3a14527c2ff21
uday <udapra@gmail.com>
2015-08-15 20:22:00 +0530

    GSS

---
+++ gssapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+from gssapp.models import Msg
+from django.utils import timezone
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def index(request):
+	if request.method == 'GET':
+		a = Msg.objects.all().order_by('date').reverse()
+		context ={'msgs':a}
+		return render(request,'gssapp/index.html',context)
+	elif request.method == 'POST':
+		t = request.POST.get("gssapp", "")
+		a = Msg(text=t,date=timezone.now())
+		a.save()
+		return redirect('/')
+
+# Create your views here.
