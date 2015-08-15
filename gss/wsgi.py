a78a3ebaf74df5291f24d625bc1f0d9641d3e499
uday <udapra@gmail.com>
2015-08-15 22:49:58 +0530

    gss3.0

--- gss/wsgi.py
+++ gss/wsgi.py
@@ -11,7 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gss.settings")
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gss.settings")
+
