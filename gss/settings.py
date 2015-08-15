51eda4dc7f4ce871e8ebe5d1a87f890414a95589
uday <udapra@gmail.com>
2015-08-15 22:45:50 +0530

    gss2.0

--- gss/settings.py
+++ gss/settings.py
@@ -124,11 +124,12 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    'gssapp/static/'
 )
